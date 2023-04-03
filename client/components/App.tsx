2a14b9e27d6d4f6d378b1f126040da01929efade
Samuel Adams <samueladams@outlook.co.nz>
2023-04-04 11:58:07 +1200

    setup draft page

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import LandingPage from './LandingPage'
 import MainLayout from './MainLayout'
 import Home from './Home'
+import UserProfilePage from './UserProfilePage'
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="register" element={<div>Register</div>} />
         <Route path="profiles" element={<div>AllProfiles </div>} />
-        <Route path="profiles/:username" element={<div>Profile</div>} />
+        <Route path="profiles/:username" element={<UserProfilePage />} />
         <Route path="post" element={<div>AddPost</div>} />
         <Route path="post/:id" element={<div>Post</div>} />
         <Route path="groups" element={<div>AllGroups</div>} />
---
+++ client/components/UserProfilePage.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+
+export default function UserProfilePage() {
+
+  const fakeUser = {
+    userImage: "../../images/avatars/ava-17.png",
+    username: "SarahRules117",
+    fullname: "Sarah McGregor",
+    location: "Auckland",
+  }
+
+
+
+  return (
+    <>
+      <div>
+        <button>View All Posts by User</button><br></br>
+        <button>View All Profiles</button>
+
+        <div>
+          {/* DISPLAY USER AVATAR  */}
+          <img src={fakeUser.userImage} alt='profileImage' />
+          {/* DISPLAY USERNAME */}
+          <p>{fakeUser.username}</p>
+
+          {/* DISPLAY FULL NAME */}
+          <p>{fakeUser.fullname}</p>
+
+          {/* LOCATION */}
+          <p>{fakeUser.location}</p>
+          {/* EDIT PROFILE BUTTON */}
+          <button>Edit Profile</button>
+        </div>
+      </div>
+    </>
+  )
+}
