ca4551b41689998dc0c055139396d0cbe93e47ff
Samuel Adams <samueladams@outlook.co.nz>
2023-04-04 15:46:55 +1200

    add db function for to get a user

---
+++ server/db/functions/users.ts
@@ -0,0 +1,9 @@
+import { User, UserSnakeCase } from '../../../models/user'
+import connection from '../connection'
+
+export function getUser(username: string, db = connection): Promise<User> {
+  return db<UserSnakeCase>('users')
+    .where({ username })
+    .select('id', 'full_name as fullName', 'location', 'image')
+    .first()
+}
