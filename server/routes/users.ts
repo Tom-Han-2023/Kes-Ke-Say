d6a83728f3b9e96f81ba20dc9a04512813569464
Samuel Adams <samueladams@outlook.co.nz>
2023-04-04 16:01:47 +1200

    add route for username

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,10 +1,23 @@
 import express from 'express'
+import { getUser } from '../db/functions/users'
 
 const router = express.Router()
 
-// GET /api/v1/posts
+// GET /api/v1/users
 router.get('/', (req, res) => {
   res.status(200).send('Hello from the users route!')
+})
+
+router.get('/:username', async (req, res) => {
+  try {
+    const user = await getUser(req.params.username)
+    res.status(200).json(user)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      error: 'there was an error trying to get the user',
+    })
+  }
 })
 
 export default router
