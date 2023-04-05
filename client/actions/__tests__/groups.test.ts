b6e8ca91ee165de06d6be3893aff8bdea1183ff1
Matiu Emery-Tohora-23 <122196130+matiu-emery@users.noreply.github.com>
2023-04-05 15:57:45 +1200

    completes all tests and everything

--- client/actions/__tests__/groups.test.ts
+++ client/actions/__tests__/groups.test.ts
@@ -1,5 +1,27 @@
 // testing our thunk action FETCHGROUPS
 import { getGroups } from '../../apiClient/groups'
+import { RootState } from '../../store'
 import * as actions from '../groups'
+import { SET_ERROR } from '../groups'
+// actions.fetchGroups
 
 jest.mock('../../apiClient/groups')
+const dispatch = jest.fn() // mock dispatch
+const getState = jest.fn(() => ({} as RootState))
+
+describe('fetchGroups', () => {
+  it('dispatches setError', () => {
+    // mocking our api call and rejects the promise
+    jest
+      .mocked(getGroups)
+      .mockImplementation(() => Promise.reject(new Error('couldnt sorry')))
+    return actions
+      .fetchGroups()(dispatch, getState)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledWith({
+          type: SET_ERROR,
+          payload: 'couldnt sorry',
+        })
+      })
+  })
+})
