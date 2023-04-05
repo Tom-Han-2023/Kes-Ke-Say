cbb3fbf7267e9986972a18aa98eacbad7de2af8f
Tom Han <121856196+Tom-Han-2023@users.noreply.github.com>
2023-04-05 13:02:30 +1200

    finish writing test for actions

--- client/actions/__tests__/weatherActions.test.ts
+++ client/actions/__tests__/weatherActions.test.ts
@@ -1,20 +1,23 @@
-import { FAILURE_WEATHER, fetchWeather, requestWeather } from '../weatherAction'
+import {
+  FAILURE_WEATHER,
+  fetchWeather,
+  requestWeather,
+  REQUEST_WEATHER,
+} from '../weatherAction'
 import { getWeather } from '../../apiClient/weather'
 import { WeatherAction } from '../weatherAction'
-
+import { RootState } from '../../store'
 jest.mock('../../apiClient/weather')
 const dispatch = jest.fn()
+const getState = jest.fn(() => ({} as RootState))
+
 describe('fetchWeather function', () => {
   it('dispatches failure on error', () => {
     jest
       .mocked(getWeather)
       .mockImplementation(() => Promise.reject(new Error('Error')))
-    
 
-    return fetchWeather()
-    (dispatch)=> {
-      dispatch(requestWeather())
-    }.then(() => {
+    return fetchWeather()(dispatch, getState).then(() => {
       expect(dispatch).toHaveBeenCalledWith({
         type: FAILURE_WEATHER,
         payload: 'Error',
