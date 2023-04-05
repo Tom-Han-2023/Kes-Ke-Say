2659796a39fd6f5dccc5cb71b8eab29e56cbbec4
Tom Han <121856196+Tom-Han-2023@users.noreply.github.com>
2023-04-06 08:35:41 +1200

    complete test

--- server/externalApi/__test__/weatherApi.test.ts
+++ server/externalApi/__test__/weatherApi.test.ts
@@ -1,9 +1,67 @@
 import 'dotenv/config'
 import { getWeatherData } from '../weatherApi'
+import nock from 'nock'
 
 describe('getWeatherData()', () => {
-  it('should return weather data from api', async () => {
+  beforeEach(() => {
+    // Mock the external API
+    const API_KEY = process.env.API_KEY
+    const location = 'Auckland'
+
+    nock('http://api.weatherapi.com')
+      .get(`/v1/current.json?key=${API_KEY}&q=${location}`)
+      .reply(200, {
+        location: {
+          name: 'Auckland',
+          region: '',
+          country: 'New Zealand',
+          lat: -36.87,
+          lon: 174.77,
+          tz_id: 'Pacific/Auckland',
+          localtime_epoch: 1680646337,
+          localtime: '2023-04-05 10:12',
+        },
+        current: {
+          last_updated_epoch: 1680645600,
+          last_updated: '2023-04-05 10:00',
+          temp_c: 17,
+          temp_f: 62.6,
+          is_day: 1,
+          condition: {
+            text: 'Sunny',
+            icon: '//cdn.weatherapi.com/weather/64x64/day/113.png',
+            code: 1000,
+          },
+          wind_mph: 3.8,
+          wind_kph: 6.1,
+          wind_degree: 120,
+          wind_dir: 'ESE',
+          pressure_mb: 1019,
+          pressure_in: 30.09,
+          precip_mm: 0,
+          precip_in: 0,
+          humidity: 83,
+          cloud: 0,
+          feelslike_c: 17,
+          feelslike_f: 62.6,
+          vis_km: 10,
+          vis_miles: 6,
+          uv: 5,
+          gust_mph: 2.5,
+          gust_kph: 4,
+        },
+      })
+  })
+
+  afterEach(() => {
+    // Clean up after the tests
+    nock.cleanAll()
+  })
+
+  it('should return weather data from API', async () => {
     const response = await getWeatherData('Auckland')
-        expect(response.location.name).toBe('Auckland')
+    expect(response.location.name).toBe('Auckland')
+    expect(response.current.temp_c).toBe(17)
+    expect(response.current.condition.text).toBe('Sunny')
   })
 })
