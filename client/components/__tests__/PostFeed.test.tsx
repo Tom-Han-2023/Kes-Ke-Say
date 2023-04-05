160e29c1b7a37ab228224d2c80bbfc7fee5c0b8f
Keri van der Westhuizen <75972814+kerivdw@users.noreply.github.com>
2023-04-05 15:50:46 +1200

    fix test error

--- client/components/__tests__/PostFeed.test.tsx
+++ client/components/__tests__/PostFeed.test.tsx
@@ -38,4 +38,35 @@ describe('<PostFeed />', () => {
     expect(loadingBanner).toBeInTheDocument()
     expect(scope.isDone()).toBeTruthy()
   })
+
+  it('should show an error', async () => {
+    const scope = nock('http://localhost')
+      .get('/api/v1/posts/')
+      .reply(500, {
+        posts: [
+          {
+            id: 1,
+            user_id: 1,
+            body: 'I found this really interesting book, you should check it out',
+            image: '',
+            created_at: 1680647489666,
+            users_username: 'paige',
+          },
+        ],
+      })
+
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Provider store={intialiseStore()}>
+          <App />
+        </Provider>
+      </MemoryRouter>
+    )
+
+    await waitFor(() => expect(screen.getByText('Post')).toBeInTheDocument())
+
+    const loadingBanner = screen.getByText('There was an error')
+    expect(loadingBanner).toBeInTheDocument()
+    expect(scope.isDone()).toBeTruthy()
+  })
 })
