4808e789bfed4afd865da3dda3282ab85e322cfb
Keri van der Westhuizen <75972814+kerivdw@users.noreply.github.com>
2023-04-05 15:30:46 +1200

    add the component tests

--- client/components/PostFeed.tsx
+++ client/components/PostFeed.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
 function PostFeed() {
-  const { data } = useAppSelector((state) => state.posts)
+  const { loading, error, data } = useAppSelector((state) => state.posts)
   const dispatch = useAppDispatch()
   const [posts, setPosts] = useState([] as Post[])
 
@@ -26,36 +26,44 @@ function PostFeed() {
 
   return (
     <>
-      <InfiniteScroll
-        dataLength={posts.length}
-        next={fetchMoreData}
-        hasMore={posts.length < data.length}
-        loader={
-          posts.length <= data.length ? (
-            <h4 className="text-center">More Posts coming...</h4>
-          ) : (
-            <h4 className="text-center">You have reached the end for today</h4>
-          )
-        }
-      >
-        <div className="container mx-auto">
-          <h2 className="text-center">Posts</h2>
-          {posts.map((post: Post) => {
-            return (
-              <div key={`key${post.id}`}>
-                <Posts
-                  id={post.id}
-                  users_username={post.users_username}
-                  body={post.body}
-                  image={post.image}
-                  created_at={post.created_at}
-                  user_id={post.user_id}
-                />
-              </div>
+      {loading ? (
+        <p>loading...</p>
+      ) : error !== undefined ? (
+        <p>There was an error</p>
+      ) : (
+        <InfiniteScroll
+          dataLength={posts.length}
+          next={fetchMoreData}
+          hasMore={posts.length < data.length}
+          loader={
+            posts.length <= data.length ? (
+              <h4 className="text-center">More Posts coming...</h4>
+            ) : (
+              <h4 className="text-center">
+                You have reached the end for today
+              </h4>
             )
-          })}
-        </div>
-      </InfiniteScroll>
+          }
+        >
+          <div className="container mx-auto">
+            <h2 className="text-center">Posts</h2>
+            {posts.map((post: Post) => {
+              return (
+                <div key={`key${post.id}`}>
+                  <Posts
+                    id={post.id}
+                    users_username={post.users_username}
+                    body={post.body}
+                    image={post.image}
+                    created_at={post.created_at}
+                    user_id={post.user_id}
+                  />
+                </div>
+              )
+            })}
+          </div>
+        </InfiniteScroll>
+      )}
     </>
   )
 }
---
+++ client/components/__tests__/PostFeed.test.tsx
@@ -0,0 +1,41 @@
+import { screen, render, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import nock from 'nock'
+import { MemoryRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import App from '../App'
+import { intialiseStore } from '../../store'
+
+describe('<PostFeed />', () => {
+  it('should show a loading banner', async () => {
+    const scope = nock('http://localhost')
+      .get('/api/v1/posts/')
+      .reply(200, {
+        posts: [
+          {
+            id: 1,
+            user_id: 1,
+            body: 'I found this really interesting book, you should check it out',
+            image: '',
+            created_at: 1680647489666,
+            users_username: 'paige',
+          },
+        ],
+      })
+
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Provider store={intialiseStore()}>
+          <App />
+        </Provider>
+      </MemoryRouter>
+    )
+
+    await waitFor(() => expect(screen.getByText('Post')).toBeInTheDocument())
+
+    const loadingBanner = screen.getByText('loading...')
+    expect(loadingBanner).toBeInTheDocument()
+    expect(scope.isDone()).toBeTruthy()
+  })
+})
