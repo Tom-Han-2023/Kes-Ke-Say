1b619f3be0b27f77137f64bbb116b059425af4c7
shiraz-00 <121928672+shiraz-00@users.noreply.github.com>
2023-04-05 21:58:11 +1200

    prep for merge

--- client/apiClient/__tests__/user.api.test.ts
+++ client/apiClient/__tests__/user.api.test.ts
@@ -4,10 +4,9 @@ import * as actions from '../../actions/index'
 jest.mock('../profileindex')
 
 test('getUserInfo', async () => {
-  jest.mocked(getUserInfo).mockResolvedValue(User)
+  jest.mocked(getUserInfo).mockResolvedValue()
   const dispatch = jest.fn()
 
   await actions.fetchUserInfo('ida')(dispatch, jest.fn())
-  console.log(dispatch.mock.calls)
-  expect(dispatch.mock.calls)
+  
 })
