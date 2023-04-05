9c20359d545175185b869f9f01d218394ac05932
Roisin <113397527+roisin-yates@users.noreply.github.com>
2023-04-05 14:27:21 +1200

    fix PostFeed location

--- client/components/MainLayout.tsx
+++ client/components/MainLayout.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Nav from './Nav'
 import Footer from './Footer'
-import PostFeed from './PostFeed'
 
 function MainLayout() {
   return (
     <>
       <Nav />
       <Outlet />
-      <PostFeed />
       <Footer />
     </>
   )
