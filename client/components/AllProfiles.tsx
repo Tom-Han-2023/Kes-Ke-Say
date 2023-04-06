3060a61ac873d980cb7c8548638472b5f480c1a6
Jamie Truman <121926321+jamie-truman@users.noreply.github.com>
2023-04-06 13:25:35 +1200

    db test file

--- client/components/AllProfiles.tsx
+++ client/components/AllProfiles.tsx
@@ -11,6 +11,7 @@ const AllProfiles = () => {
     dispatch(getAllProfiles())
   }, [dispatch])
 
+  
   return (
 
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
